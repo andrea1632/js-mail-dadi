2ba7cbb47ebf55c2503e4ff29f78cb17b6ebaf00
andrea1632 <andrea.bellucci10@libero.it>
2022-03-15 17:11:12 +0100

    fix

--- Mail/main.js
+++ Mail/main.js
@@ -3,13 +3,14 @@ let mailTotali = ["andrea@gmail.it", "luca@gmail.it", "alessandro@gmail.it", "jo
 mailUtente = prompt("inserisci la tua mail");
 console.log(mailUtente);
 let content = document.getElementById("email");
+let mailTrovata = false;
 for (var i = 0; i <= mailTotali.length; i++) {
     if (mailUtente == mailTotali[i]){
-        console.log("la tua mail è già registrata")
-        content.innerHTML += `<h1>la tua mail è già registrata</h1>`
+        mailTrovata = true;        
     }
-    else{
-        console.log("vai a registrarti")
-        content.innerHTML += `<h1>vai a registrarti</h1>`
-    }
+}
+if (mailTrovata == true){
+    content.innerHTML += `<h1>la tua mail è già registrata</h1>`
+} else{
+    content.innerHTML += `<h1>vai a registrarti</h1>`
 }
