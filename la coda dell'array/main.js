83dea14de9ca367ab410e9d84d1a04c54801f8e3
andrea1632 <andrea.bellucci10@libero.it>
2022-03-15 16:30:40 +0100

    esercizio completo

--- la coda dell'array/main.js
+++ la coda dell'array/main.js
@@ -5,11 +5,17 @@ if (isNaN(numeriDesiderati)){
 numeriDesiderati = parseInt(numeriDesiderati);
 let numeriTotali = [];
 let content = document.getElementById("content");
+let numeriSottratti = prompt("quanti numeri vuoi togliere?");
+let contentSottrati = document.getElementById("numeriSottratti")
 
 for (let i = 0; i <= numeriDesiderati -1; i++){
     let numeroRandom = Math.floor(Math.random() * 99 + 1);
     numeriTotali.push(numeroRandom);
     console.log(numeriTotali);
-    content.innerHTML += `  ${numeriTotali[i]}`
-    console.log(numeroRandom)
+    content.innerHTML += `  ${numeriTotali[i]}`;
 }
+for (let i = 1; i <= numeriSottratti; i++){
+    numeriTotali.pop()
+}
+console.log(numeriTotali)
+contentSottrati.innerHTML += `${numeriTotali}`
